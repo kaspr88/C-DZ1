a451c0b1678115b35090c179508e7355340b5d7f
Igor Bylinkin <kaspr88@gmail.com>
2022-12-19 11:46:36 +0600

    One program

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int Number1 = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите второе число");
+int Number2 = int.Parse(Console.ReadLine() ?? "");
+if (Number1 > Number2)
+{
+    Console.WriteLine($"{Number1}>{Number2}");
+    Console.WriteLine($"Max={Number1}");
+}
+else
+{
+    Console.WriteLine($"{Number1}<{Number2}");
+    Console.WriteLine($"Max={Number2}");
+
+};
