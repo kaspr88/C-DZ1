52966d09324d4f2f6c2680fdcad3c2d208b2b339
Igor Bylinkin <kaspr88@gmail.com>
2022-12-19 16:38:16 +0600

    Four program

---
+++ Task 4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число:");
+int number = int.Parse(Console.ReadLine() ?? "");
+int count = 2;
+while (count <= number)
+{
+    if (count % 2 == 0) Console.Write($"{count} ");
+    count++;
+}
